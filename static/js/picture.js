96adcc80ed3214a2c55fba412ae498ad02ded95f
sasha <sanchopan@inbox.ru>
2021-02-08 14:52:03 +0500

    error

--- static/js/picture.js
+++ static/js/picture.js
@@ -5,7 +5,7 @@ $(function(){
         $(this).attr('src', $(this).attr('gifi'))
     else if ($(this).attr("src") == $(this).attr("gifi"))
         {
-        //$(this).attr('src', $(this).attr('minim'));
+        $(this).attr('src', $(this).attr('minim'));
 
         $('body').append('<div id="overlay"></div><div id="magnify"><img src="'+i_path+'"><div id="close-popup"><i></i></div></div>');
         $('#magnify').css({
